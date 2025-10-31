4429b7eb0c68890eb5bbdbb81c8cbc2e9fb308c3
Arun Karthik AnandKumar <137405963+ArunKarthikAnandKumar@users.noreply.github.com>
2025-10-31 11:00:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors=require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var Router=require('./routes/userRoutes')
+var authRouter=require('./routes/authRoutes')
+var adminRouter=require('./routes/adminRoutes')
+
+
+
+
+const userModel=require('./model/users')
+
+var dotenv=require('dotenv')
+dotenv.config()
+
+var requestLoger=require('./utilites/requestLogger')
+var errorLogger=require('./utilites/errorLogger')
+var {isAdmin,isAuthenticated}=require('./utilites/authMiddleware')
+
+const Auth=require('./utilites/auth')
+
+let connectDb=require('./config/db')
 
 var app = express();
 
@@ -13,29 +31,37 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(requestLoger)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use('/assets/uploads/continents',express.static(path.join(__dirname,'/assets/uploads/continents')))
+app.use('/assets/uploads/country',express.static(path.join(__dirname,'/assets/uploads/country')))
+app.use('/assets/uploads/state',express.static(path.join(__dirname,'/assets/uploads/state')))
+app.use('/assets/uploads/city',express.static(path.join(__dirname,'/assets/uploads/city')))
+app.use('/assets/uploads/attractions',express.static(path.join(__dirname,'/assets/uploads/attractions')))
+app.use('/assets/uploads/restaurants',express.static(path.join(__dirname,'/assets/uploads/restaurants')))
+app.use('/assets/uploads/hotels',express.static(path.join(__dirname,'/assets/uploads/hotels')))
+app.use('/assets/uploads/destinationGuides',express.static(path.join(__dirname,'/assets/uploads/destinationGuides')))
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use('/api/auth',isAuthenticated,authRouter)
+app.use('/api/admin',adminRouter)
+app.use('/api',Router)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+
+app.listen(process.env.PORT,async ()=>{
+  console.log("travelTroveBacken Running on localhost:3000/")
+  await connectDb()
+
+ 
+
+})
+
+app.use(errorLogger)
 
 module.exports = app;
